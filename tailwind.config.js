60d8c764f1a83b384c2bbe0221eab6b2e60a55be
Your Name <you@example.com>
2024-06-24 10:25:21 -0500

    segundo commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,49 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: [
+	'./src/**/*.html',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+	  extend: {
+		  backgroundImage: {
+			  'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+			  'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+			  'yosemite': "url('../img/yosemite.jpg')",
+			  'LA': "url('../img/LA.jpg')",
+			  'seattle': "url('../img/seattle.jpg')",
+			  'new_york': "url('../img/new_york.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'sydney': "url('../img/sydney.jpg')",
+			  'miami': "url('../img/miami.jpg')",
+			  'switzerland': "url('../img/switzerland.jpg')",
+			  'bali': "url('../img/bali.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'chicago': "url('../img/chicago.jpg')",
+			  'europe': "url('../img/europe.jpg')",
+			  'iceland': "url('../img/iceland.jpg')",
+			  
+			},
+			backgroundColor: theme =>({
+				...theme('colors'),
+				'primary': '#CC2D4A',
+				'secondary': '#8FA206',
+				'terciary': '#61AEC9',
+			}),
+			textColor:{
+				'primary': '#CC2D4A',
+				'secondary': '#8FA206',
+				'terciary': '#61AEC9',
+			},
+			fontFamily: {
+				Montserrat: ["Montserrat", "sans-serif"],
+			 },
+			
+		},
+	},
+	variants: {
+		width: ["responsive", "hover", "focus"],
+		extend: {
+	 },
   },
   plugins: [],
 }
-
